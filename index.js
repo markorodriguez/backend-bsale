d1a5009dee65ba1720f00a4575e024b45997d8d2
unknown <marko.rodriguez@hotmail.es>
2022-06-06 20:06:45 -0500

    cors configuration

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
- });
+
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",
