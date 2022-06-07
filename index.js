e68f3a5a2d61f6d2a0b7ba4e25f2ccbede8bbfdc
unknown <marko.rodriguez@hotmail.es>
2022-06-06 20:25:35 -0500

    minor changes

--- index.js
+++ index.js
@@ -4,23 +4,8 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
-//app.use(cors());
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-  })
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
